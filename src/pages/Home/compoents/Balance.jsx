abb0b1b4d9afeed6719279f1c02e7bd72230567e
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-09-22 21:28:41 +0800

    [FEAT] add balacne group by user and show by acordian

--- src/pages/Home/compoents/Balance.jsx
+++ src/pages/Home/compoents/Balance.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import constants from '../../../global/constants';
 import { ListGroup, Accordion } from 'react-bootstrap';
 import { GroupInfo } from './Home';
+import Icons from '../../../global/Icons';
 
 const Balance = ({ isDebtChanged }) => {
   console.log('@balance');
@@ -50,27 +51,46 @@ const Balance = ({ isDebtChanged }) => {
                 </ListGroup.Item>
               );
             })
-          : balances.map((balance) => {
+          : balances.map((userBalance) => {
               return (
                 <Accordion>
-                  <Accordion.Item
-                    key={balance.id}
-                    id={balance.id}
-                    className="item"
-                    eventKey="1"
-                    // onClick={(event) => {
-                    //   handleExtend(event, balance.id);
-                    // }}
-                  >
-                    <Accordion.Header id={balance.id}>
+                  <Accordion.Item key={userBalance.uid} id={userBalance.uid} className="item" eventKey="1">
+                    <Accordion.Header id={userBalance.uid}>
                       <div className="group-balance-list">
-                        <span>{groupUserNames[balance.borrower]} </span>
-                        <span>own </span>
-                        <span>{groupUserNames[balance.lender]} </span>
-                        <span>${balance.amount}</span>
+                        <Icons.UserIcon />
+                        <span>{groupUserNames[userBalance.uid]} </span>
+                        {userBalance.balance > 0 ? (
+                          <>
+                            <span>paid </span>
+                            <span className="owned-font">{`NT$ ${userBalance.balance}`} </span>
+                          </>
+                        ) : (
+                          <>
+                            <span>owns</span>
+                            <span className="own-font">{`NT$ ${-userBalance.balance}`} </span>
+                          </>
+                        )}
                       </div>
                     </Accordion.Header>
-                    <Accordion.Body></Accordion.Body>
+                    <Accordion.Body>
+                      <ListGroup variant="flush" className="group-balance-detail-list">
+                        {userBalance.detail.map((detail) => {
+                          return detail.borrower === userBalance.uid ? (
+                            <ListGroup.Item key={detail.id} className="group-balance-detail-item">
+                              <div>owns</div>
+                              <div>{`${CurrGroupInfo.groupUserNames[detail.lender]}`}</div>
+                              <div className="own-font">{`NT$ ${detail.amount}`}</div>
+                            </ListGroup.Item>
+                          ) : (
+                            <ListGroup.Item key={detail.id} className="group-balance-detail-item">
+                              <div>owned by</div>
+                              <div>{`${CurrGroupInfo.groupUserNames[detail.borrower]}`}</div>
+                              <div className="owned-font">{`NT$ ${detail.amount}`}</div>
+                            </ListGroup.Item>
+                          );
+                        })}
+                      </ListGroup>
+                    </Accordion.Body>
                   </Accordion.Item>
                 </Accordion>
               );
